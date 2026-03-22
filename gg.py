c0343ae3fd33e9ae0b3580b66fc62a7bb0ced210
Sidharth Falodia <189434527+Sidharth-max@users.noreply.github.com>
2026-03-22 20:29:21 +0530

    Remove secrets and node_modules from tracking

--- gg.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-from twilio.rest import Client
-
-load_dotenv()
-
-client = Client(os.getenv("TWILIO_ACCOUNT_SID"), os.getenv("TWILIO_AUTH_TOKEN"))
-number = client.incoming_phone_numbers.list()[0]
-print(number.capabilities) 
